09c559c45e3b2fa71edc74889155cf9470271bad
Florens Pauwels <github@florens.be>
2020-08-02 21:14:46 +0200

    Change license

--- src/main/java/curiousarmorstands/CuriousArmorStands.java
+++ src/main/java/curiousarmorstands/CuriousArmorStands.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2020  Florens Pauwels
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package curiousarmorstands;
 
 import nerdhub.cardinal.components.api.event.EntityComponentCallback;
@@ -32,9 +49,8 @@ public class CuriousArmorStands implements ModInitializer {
     @Override
     public void onInitialize() {
         UseEntityCallback.EVENT.register(CuriousArmorStands::onUseEntity);
-        EntityComponentCallback.event(ArmorStandEntity.class).register((armorStandEntity, componentContainer) -> {
-            componentContainer.put(CuriosComponent.INVENTORY, new ArmorStandCuriosComponent(armorStandEntity));
-        });
+        EntityComponentCallback.event(ArmorStandEntity.class).register((armorStandEntity, componentContainer) ->
+                componentContainer.put(CuriosComponent.INVENTORY, new ArmorStandCuriosComponent(armorStandEntity)));
     }
 
 /*    @SubscribeEvent
